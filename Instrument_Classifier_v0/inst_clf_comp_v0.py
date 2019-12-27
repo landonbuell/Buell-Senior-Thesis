dd0b2022a3fb2d9999f6ad3ffd6e75e57537c63b
landonbuell <lhb1007@wildcats.unh.edu>
2019-12-27 13:27:35 -0500

    Initial commit

---
+++ Instrument_Classifier_v0/inst_clf_comp_v0.py
@@ -0,0 +1,33 @@
+"""
+Landon Buell
+Instrument Classifier v0
+Compsosite Level Functions
+27 December 2019
+"""
+
+        #### IMPORTS ####
+
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import inst_clf_func_v0 as func_v0
+
+        #### COMPOSITE FUNCTIONS ####
+
+def Waveform_Classify ():
+    """
+    Classify Musical Instruments Based on Wavefoms Properties 
+    """
+    pass
+
+def Frequenecy_Classify ():
+    """
+    Classify Musical Instruments Based on Frequency Spectra Properties 
+    """
+    pass
+
+def Frequenecy_Classify ():
+    """
+    Classify Musical Instruments Based on Spectrogram Properties 
+    """
+    pass
---
+++ README.md
@@ -0,0 +1,2 @@
+# Buell-Senior-Thesis
+ Code Repository For all programs related to Landon's BS Physics Senior thesis (PHYS 799)
