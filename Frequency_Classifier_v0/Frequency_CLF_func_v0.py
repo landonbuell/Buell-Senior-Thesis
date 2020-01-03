5e9ddf7f2ffa1bead5b138a66f1b50eb58f1168c
landonbuell <lhb1007@wildcats.unh.edu>
2020-01-03 00:35:39 -0500

    2 January 2020
    - Begun Classifier Objects
    - Program stable, outputs confusion matrix for each classifier
    - Analysis soon to follow
    - Labeled each class 0 to 18 for 19 instrument classes

--- Frequency_Classifier_v0/Frequency_CLF_func_v0.py
+++ Frequency_Classifier_v0/Frequency_CLF_func_v0.py
@@ -8,6 +8,7 @@ Functions
             #### IMPORTS ####
 
 import numpy as np
+import matplotlib.pyplot as plt
 import pandas as pd
 import os
 
@@ -27,6 +28,40 @@ def read_directory(dir,ext):
                 output = np.append(output,file)
     return output                           # return the list of files
 
+def read_csvfile (filename,disp=True):
+    """
+    Load data set from directory 
+    --------------------------------
+    filename (str) : string to identify column to use
+    --------------------------------
+    Returns matrix of CSV data
+    """
+    frame = pd.read_csv(filename,sep='\t',header=0)     # read CSV file
+    frame.drop(frame.columns[0],axis=1,inplace=True)    # drope 1st column
+    frame = frame.astype(dtype=float)                   # cast to floats
+    if disp == True:                        # print output?
+        print("\tDataFrame information:")
+        print("\t\tSamples:",frame.shape[0])
+        print("\t\tFeatures:",frame.shape[1])
+    return frame                            # return the frame
+
+def save_csvfile (filename,data,classes):
+    """
+    Load data set from directory 
+    --------------------------------
+    filename (str) : string to write file to
+    data (arr) : N x N array of data to make into DataFrame
+    classes (arr) : 1 x N array of class value indentifiers
+    --------------------------------
+    Returns matrix of CSV data
+    """
+    cols = ['Predicted: '+str(x) for x in classes]
+    rows = ['Observed: '+str(x) for x in classes]
+    frame = pd.DataFrame(data,index=rows,columns=cols,dype=float)
+    frame.to_csv(filename,sep='\t',header=True,
+                 index=Flase,mode='w')
+    return frame
+
 def random_split (xdata,ydata,size=0.1,state=0):
     """
     Split dataset into training and testing data sets based on random seed
@@ -38,13 +73,13 @@ def random_split (xdata,ydata,size=0.1,state=0):
     --------------------------------
     Returns dictionary of train vs. test & x vs y data sets
     """ 
-    xtrain,xtest = model.train_test_split(xdata,testsize=size,random_state=state)
-    ytrain,ytest = model.train_test_split(xdata,testsize=size,random_state=state)
+    xtrain,xtest = model.train_test_split(xdata,test_size=size,random_state=state)
+    ytrain,ytest = model.train_test_split(ydata,test_size=size,random_state=state)
     data_dict = {'xtrain':xtrain,'xtest':xtest,
                  'ytrain':ytrain,'ytest':ytest}
     return data_dict                # return the dictionary of data sets
 
-def startified_split (xdata,ydata,size=0.1,state=0):
+def stratified_split (xdata,ydata,size=0.1,state=0):
     """
     Split dataset into training and testing data sets based on Stratification
     --------------------------------
@@ -68,15 +103,40 @@ def SGD_Classifier (name,xtrain,ytrain,state=None):
     --------------------------------
     name (str) : Name to attatch to classifer for human identification
     xtrain (arr) : N x M array of training data - N samples, M features 
-    ydata (arr) : 1 x N array of training target classification values 
+    ytrain (arr) : 1 x N array of training target classification values 
     state (int) : random state for classifier object (None by default)
     --------------------------------
     Return SGD Classifier
     """
-    clf = SGDClassifier(random_state=state)         # create classifer
+    clf = SGDClassifier(random_state=state,average=19)       
     setattr(clf,'name',name)                        # attach name of obj
     clf.fit(xtrain,ytrain)                          # train the classifier
     return clf
+
+def confusion_matrix (clf,xdata,ydata,disp=True):
+    """
+    Compute confusion matrix for N-Classes Classifier object
+    ----------------
+    clf (classifier obj) : Classifier object to build confusion matrix for
+    xdata (array/DataFrame) : x-training dataset
+    ydata (array/DataFrame) : y-training target dataset
+    disp (bool) : Visualize Confusion matrix (True by default)
+    ----------------
+    returns Confusion matrix
+    """
+    ypred = model.cross_val_predict(clf,xdata,ydata)    # cross-val prediction
+    conf_mat = metrics.confusion_matrix(ydata,ypred)    # build conf matrix
+    if disp == True:
+        axes = np.arange(0,19)          # array for axes
+        plt.matshow(conf_mat,cmap=plt.cm.gray)      # disp.matrix
+        title = 'Confusion_Matrix_'+str(clf.name)   # title for figure
+        plt.title(title,size=12,weight='bold')      # set title
+        plt.xticks(axes)            # set x ticks
+        plt.yticks(axes)            # set y ticks
+        plt.tight_layout()          # apply tight layout
+        plt.savefig(title+'.png')   # save to directory
+        #plt.show()       
+    return conf_mat                 # return matrix
 
 def general_metrics (clf,xdata,ydata,disp=True):
     """
@@ -94,10 +154,10 @@ def general_metrics (clf,xdata,ydata,disp=True):
     precision = metrics.precision_score(ydata,ypred)    # compute precision score
     recall = metrics.recall_score(ydata,ypred)          # compute recall score
     f1 = metrics.f1_score(ydata,ypred)                  # compute f1 score
-
-    if disp == True:                        # print output to line?
-        print('Precision Score:',precision)
-        print('Recall Score:',recall)
-        print("F1 Score:",f1)
-
-    return precision,recall,f1                  # return values
+    metricsdict = {'Precision Score':precision,
+                   'Recall Score':recall, 'F1 Score':f1}
+    if disp == True:                                # print output to line?
+        print("\tClassifier Metrics")               # header
+        for keys,vals in metricdict.items():        # for each key value pair:
+            print('\t\t'+str(keys),':',str(vals))   # print metric out
+    return metricsdict
--- Frequency_Classifier_v0/Frequency_CLF_main_v0.py
+++ Frequency_Classifier_v0/Frequency_CLF_main_v0.py
@@ -6,7 +6,6 @@ Main Functions
 """
 
 import numpy as np
-import matplotlib.pyplot as plt
 import os
 import time
 import Frequency_CLF_func_v0 as CLF_func
@@ -18,21 +17,33 @@ if __name__ == '__main__':
     int_dir = 'C:/Users/Landon/Documents/GitHub/Buell-Senior-Thesis/Frequency_Classifier_v0'
     readdir = 'C:/Users/Landon/Documents/wav_data/Frequency_Bands'
     classifiers = []                                # list to hold classifier objs
-    labels = np.loadtxt('Instrument_labels_double.txt',
-                        dtype=int,delimiter='\t',skiprows=1,usecols=-1)
+    labels = np.loadtxt('Instrument_labels.txt',
+                        dtype=float,delimiter='\t',skiprows=1,usecols=-1)
     filenames = CLF_func.read_directory(readdir,'.txt')
-    print("Number of ")
+
+    print("Number of Sample Labels:",len(labels))
+    print("Number of files:",len(filenames))
+    print("-"*32)
 
             #### MOVE THROUGH DATASETS ####
-    os.chdir(readdir)                                       # move to directory
-    for file in filenames:                                  # each file
-        clfname = str(file).replace('.txt','')              # clf name
-        print("Classifier name:",clfname)                   # print name
-        xdata = np.loadtxt(file,dtype=str,delimiter='\t',
-                           skiprows=1,unpack=True)          # read data from CSV
-        filenames = xdata[0]                                # instrument names
-        xdata = np.array(xdata[1:],dtype=float)             # xdata 
+    os.chdir(readdir)                               # move to directory
+    for file in filenames:                          # each file
+        clfstart = time.process_time()
+        clfname = str(file).replace('.txt','')      # clf name
+        print("\nClassifier name:",clfname)           # print name
+        
+        X = CLF_func.read_csvfile(file,True)        # read dataframe
+        datadict = CLF_func.random_split(X,labels,size=0,state=None)
+        xtrain,ytrain = datadict['xtrain'],datadict['ytrain']
+        classifier = CLF_func.SGD_Classifier(clfname,
+                    xtrain,ytrain,state=0)
+        conf_mat = CLF_func.confusion_matrix(classifier,X,labels,True)
 
+        clfend = time.process_time()
+        print("\tClassifier Time:",clfend-clfstart) # time to run classifier
+
+    os.chdir(int_dir)                               # starting path
+    print("Program Time:",time.process_time())      # print time
         
     
         
