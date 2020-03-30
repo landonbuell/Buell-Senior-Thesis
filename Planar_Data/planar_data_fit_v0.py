9d396ad41ff8116c2db68e36edb355b86d0e26a7
landonbuell <lhb1007@wildcats.unh.edu>
2020-03-30 13:22:34 -0400

    30 March 2020
    - Added function to scale columns of train/test data
    - Rand again w/ ReLu activation

--- Planar_Data/planar_data_fit_v0.py
+++ Planar_Data/planar_data_fit_v0.py
@@ -18,24 +18,41 @@ import InputTrainingData_Planar_w_Noise as planardata
 
             #### FUNCTIONS ####
 
-def testing_data (N_samples):
-    """ Create sample of 'true' data based on linear equation """
-    x = np.random.random(size=N_samples)*10
-    y = np.random.random(size=N_samples)*10
-    c = np.ones(N_samples)
-    features = np.array([c,x,y]).transpose()
-    targets = 0.3 + 0.4*x + 0.5*y
-    return features,targets
-
 def percent_errors (approx,exact):
     """ Compute the percent error of arrays """
     return (approx - exact)/exact
+
+def scale_matrix (matrix):
+    """ Normalize Columns of matrix s.t. max(col) = 1 """
+    if matrix.ndim > 1:                 # more than 1D
+        matrix = np.transpose(matrix)   # transpose
+        for row in matrix:              # each row
+            row = row/np.max(row)       # divide row by maximum of row
+        return matrix.transpose()       # return the transpose
+    else:
+        matrix = matrix/np.max(matrix)  # divide by max
+        return matrix
+
+def testing_data (N_samples,scale=True):
+    """ Create sample of 'true' data based on linear equation """
+    x = np.random.random(size=N_samples)
+    y = np.random.random(size=N_samples)
+    c = np.ones(N_samples)
+    features = np.array([c,x,y]).transpose()
+    targets = 0.3 + 0.4*x + 0.5*y
+    if scale == True:
+        features = scale_matrix(features)
+        targets = scale_matrix(targets)
+    return features,targets
 
 if __name__ == '__main__':
 
     # Load in datasets
     X_train = planardata.train_inputs
     y_train = planardata.train_outputs.ravel()
+
+    X_train = scale_matrix(X_train)     # scale the training data
+    y_train = scale_matrix(y_train)     # scale the training data
 
     output_matrix = np.array([])        # this will be written out to CSV
 
@@ -72,7 +89,7 @@ if __name__ == '__main__':
                          columns=['A Predicted','B Predicted','C Predicted',
                                   'A Error','B Error','C Error',
                                   'MSE','Train Time'])
-    frame.to_csv('Planar_Fit_ReLU.csv')
+    frame.to_csv('Planar_Fit_ReLU_scaled.csv')
 
         
     
