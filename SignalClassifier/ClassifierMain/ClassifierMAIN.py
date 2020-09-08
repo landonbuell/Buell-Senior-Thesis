186d80bd1dc2f698871389c609e94820e6b542c5
landonbuell <lhb1007@wildcats.unh.edu>
2020-09-08 00:25:15 -0400

    Added "SignalClassifier" Solution
    - Added & Renamed scripts from main Classifier

---
+++ SignalClassifier/ClassifierMain/ClassifierMAIN.py
@@ -0,0 +1,62 @@
+"""
+Landon Buell
+PHYS 799
+Instrument Classifier v0
+10 June 2020
+"""
+
+            #### IMPORTS ####
+
+import os
+import sys
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+import ModeUtilities as mode_utils
+import SystemUtilities as sys_utils
+import NeuralNetworkUtilities as NN_utils
+
+            #### MAIN EXECUTABLE ####
+
+if __name__ == '__main__':
+       
+    parent = 'C:\\Users\\Landon\\Documents\\GitHub\\Buell-Senior-Thesis\\Instrument_Classifier_v0'
+    read = os.path.join(parent,'Target-Data')
+    model = os.path.join(parent,'Model-Data')
+    export = os.path.join(parent,'Output-Data')
+
+    # PRE-PROCESSING FOR PROGRAM
+    ProgramSetup = sys_utils.ProgramInitializer(read,model,export,'train-predict',True)    
+    FILEOBJECTS,N_classes = ProgramSetup.__Call__()
+    exportpath = ProgramSetup.exportpath
+    timestart = ProgramSetup.starttime
+
+    # SETUP NEURAL NETWORK MODELS
+    NeuralNetwork = NN_utils.NetworkContainer(NN_utils.modelName,N_classes,ProgramSetup.modelpath,
+                        NN_utils.inputShapeCNN,NN_utils.inputShapeMLP,ProgramSetup.new_models)
+    modelName = NeuralNetwork.name
+    
+    # DETERMINE WHICH MODE TO RUN PROGRAM
+    if ProgramSetup.program_mode == 'train':
+        ProgramMode = mode_utils.TrainMode(FILEOBJS=FILEOBJECTS,modelName=modelName,
+                                            n_classes=N_classes,timestamp=timestart,exportpath=exportpath,
+                                            show_summary=True,groupSize=256,n_iters=1)
+    elif ProgramSetup.program_mode == 'train-predict':     
+        ProgramMode =  mode_utils.TrainPredictMode(FILEOBJS=FILEOBJECTS,modelName=modelName,
+                                            n_classes=N_classes,timestamp=timestart,exportpath=exportpath,
+                                            show_summary=False,groupSize=256,n_iters=1,testSize=0.1)
+    elif ProgramSetup.program_mode == 'predict':
+        ProgramMode = mode_utils.PredictMode(FILEOBJS=FILEOBJECTS,modelName=modelName,
+                                            n_classes=N_classes,timestamp=timestart,exportpath=exportpath,
+                                            show_summary=True,groupSize=32,labels_present=False)
+    else:
+        print("\n\tError! - Unsupported mode type")
+
+    #EXECUTE PROGRAM
+    ProgramMode.__Call__(NeuralNetwork)      
+
+    print("=)")
+    
+
+
+        
+    
---
+++ SignalClassifier/ClassifierMain/NeuralNetworkUtilities.py
@@ -0,0 +1,209 @@
+"""
+Landon Buell
+PHYS 799
+Instrument Classifier v0
+10 June 2020
+"""
+
+            #### IMPORTS ####
+
+import numpy as np
+import os
+
+import tensorflow as tf
+import tensorflow.keras as keras
+
+"""
+NeuralNetworkModels.py - "Neural Network Models"
+    Contains Definitions to wrap and contain creation of
+    Tensorflow/Keras Sequential Neural Network Models
+"""
+
+            #### VARIABLE DECLARATIONS ####
+
+modelName = "VISION"
+inputShapeMLP = (23,)
+inputShapeCNN = (560,256,1)
+
+class NetworkContainer:
+    """
+    Object that creates and contains Neural Network Model objects
+    --------------------------------
+    modelName (str) : user-ID-able string to indicate model
+    n_classes (int) : number of discrete classes for models
+    path (str) : Local parent path object where models are stored
+    inputAShape (iter) : list-like of ints giving shape of CNN branch input shape
+    inputBShape (iter) : list-like of ints giving shape of MLP branch input shape   
+    new (bool) : If true, new Neural Networks are overwritten
+    --------------------------------
+    Return Instantiated Neural Network Model Object
+    """
+    def __init__(self,modelName,n_classes,path,inputAShape,inputBShape,new=True):
+        """ Instantiate Class Object """
+        self.name = modelName
+        self.shapeA = inputAShape
+        self.shapeB = inputBShape
+
+        self.n_classes = n_classes  # number of output classes
+        self.parentPath = os.path.join(path,self.name)  # Set parent path for models      
+        self.newModel = new         # create new models?
+        
+        if self.newModel == True:               # create new networks?
+            self.MODEL = self.CreateNewModel()  # create new
+            self.SaveModel()                    # save locally
+        else:                                   # load exisitng networks
+           self.MODEL = self.LoadExistingModel()# load model
+           self.n_classes = self.MODEL.get_layer(index=-1).output_shape[-1]
+        assert self.n_classes is not None       # make sure we know how many classes
+
+    def __repr__(self):
+        """ Return string representation of NetworkContainer Class """
+        return "-"
+        
+    def CreateNewModel (self):
+        """
+        Create New Neural network to specified Parameters
+        --------------------------------
+        * no args
+        --------------------------------
+        Return Instantiated Neural Network Model
+        """
+        return NeuralNetworkModels.MultiInputNetwork(self.name,
+                            self.shapeA,self.shapeB,self.n_classes)
+        
+    def LoadExistingModel (self):
+        """
+        Load exisitng Neural network to specified Parameters
+        --------------------------------
+        * no args
+        --------------------------------
+        Return Instantiated Neural Network Model
+        """
+        try:
+            model = keras.models.load_model(self.parentPath)   # load stored Model
+            print("\n\tSuccessfully loaded model:",self.name,"from\n\t\t",self.parentPath)
+        except:
+            print("\n\tERROR! - Could not find model:",self.name,"at:\n\t\t",self.parentPath)
+            raise FileNotFoundError()
+        return model
+
+    def SaveModel(self):
+        """ 
+        Store Current Model to local disk for future Use
+        --------------------------------
+        * no args
+        --------------------------------
+        Return Instantiated Neural Network Model
+        """
+        print("\tSaving Model",self.MODEL.name,"at:\n\t\t",self.parentPath)
+        try:
+            keras.models.save_model(self.MODEL,self.parentPath,overwrite=True)
+            print("\n\tSuccessfully saved model:",self.name,"to\n\t\t",self.parentPath)
+        except:
+            print("\n\tERROR! - Could not save model:",self.name,"at:\n\t\t",self.parentPath)
+            raise FileNotFoundError()
+        return self
+
+
+class NullLayer (keras.layers.Layer):
+    """
+    Null Layer for Neural network, does nothing
+    """
+    def __init__(self,name):
+        """ Initialize Class Object Instance """
+        super(NullLayer,self).__init__(trainable=False,name=name)
+
+    def __repr__(self):
+        """ Return string representation of NullLayer Class """
+        return "NullLayer Class returns unmodifed input 'X' "
+
+    def Call (self,X):
+        """ Call Null Layer Object """
+        return X
+
+class NeuralNetworkModels:
+    """
+    Container for Neural network model architectures
+    --------------------------------
+    * no args
+    --------------------------------
+    High-level wrapper for Tensorflow Sequential models
+    """
+
+    @staticmethod
+    def MultilayerPerceptron(inputShape,outputShape,neurons=[64,64]):
+        """
+        Create MultiLayer Perceptron branch for Neural Network
+        --------------------------------
+        inputShape (iter) : Iterable of ints indicating shape input array
+        outputShape (int) : Number of unique output classes
+        neurons (iter) : Iterable of ints. i-th elem is number of nodes in i-th dense layer
+        --------------------------------
+        return un-complied MLP model
+        """
+        networkInput = keras.layers.Input(shape=inputShape,name='inputMLP')
+        x = NullLayer(name='N1_')(networkInput)
+        for i,nodes in enumerate(neurons):
+            x = keras.layers.Dense(units=nodes,activation='relu',
+                                   name='D'+str(i+1)+'_')(x)
+        x = keras.Model(inputs=networkInput,outputs=x,name="MultilayerPerceptron")
+        return x
+
+    @staticmethod
+    def ConvolutionalNeuralNetwork2D (inputShape,outputShape,filterSizes=[32,32],
+                    kernelSizes=[(3,3),(3,3)],poolSizes=[(3,3),(4,4)],neurons=[128]):
+        """
+        Create 2d Convolutional Neural Network branch for Neural Network
+        --------------------------------
+        inputShape (iter) : Iterable of ints indicating shape input array
+        outputShape (int) : Number of unique output classes
+        filtersSizes (iter) : Iterable of ints. i-th elem is n_filters in i-th conv layer group
+        kernelSizes (iter) : Iterable of ints. i-th elem is kernel size in i-th layer group  
+        neurons (iter) : Iterable of ints. i-th elem is number of nodes in i-th dense layer
+        --------------------------------
+        return un-complied CNN model
+        """
+        assert len(filterSizes) == len(kernelSizes)
+        networkInput = keras.layers.Input(shape=inputShape,name="inputCNN")
+        x = NullLayer(name='N1')(networkInput)
+        for i,(filters,kernel,pool) in enumerate(zip(filterSizes,kernelSizes,poolSizes)):
+            x = keras.layers.Conv2D(filters=filters,kernel_size=(kernel),activation='relu',name='C'+str(i+1)+'A')(x)
+            x = keras.layers.Conv2D(filters=filters,kernel_size=(kernel),activation='relu',name='C'+str(i+1)+'B')(x)
+            x = keras.layers.MaxPool2D(pool_size=pool,name='P'+str(i+1))(x)
+
+        x = keras.layers.Flatten()(x)          
+        for i,nodes in enumerate(neurons):
+            x = keras.layers.Dense(units=nodes,activation='relu',
+                                   name='D'+str(i+1))(x)
+        x = keras.Model(inputs=networkInput,outputs=x,name="ConvolutionalNetwork2D")
+        return x
+
+
+    @staticmethod
+    def MultiInputNetwork (name,inputA,inputB,n_classes):
+        """
+        Create Multi-Input layer neural Network
+        --------------------------------
+        name (str) : Name to use for neural network
+        inputA (iter) : list-like of ints indicating input shape of CNN branch
+        inputA (iter) : list-like of ints indicating input shape of MLP branch
+        n_classses (int) : Number of unique output classes
+        --------------------------------
+        Return complied tf.keras model
+        """
+        modelCNN = NeuralNetworkModels.ConvolutionalNeuralNetwork2D(inputA,n_classes,
+                        filterSizes=[32,32,32],kernelSizes=[(3,3),(3,3),(3,3)],
+                        poolSizes=[(3,3),(3,3),(3,3)],neurons=[64,64])
+        modelMLP = NeuralNetworkModels.MultilayerPerceptron(inputB,n_classes)
+
+        x = keras.layers.concatenate([modelCNN.output,modelMLP.output])
+
+        x = keras.layers.Dense(units=n_classes,activation='softmax',name='Output')(x)
+        modelMain = keras.Model(inputs=[modelCNN.input,modelMLP.input],outputs=x)
+
+        modelMain.compile(optimizer=keras.optimizers.Adam(learning_rate=0.001,
+                                        beta_1=0.9,beta_2=0.999,epsilon=1e-07),
+                            loss=keras.losses.CategoricalCrossentropy(),
+                            metrics=['Precision','Recall'])
+        return modelMain
+
