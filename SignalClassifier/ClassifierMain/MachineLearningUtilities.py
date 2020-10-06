f61fbe3b469003957565375f1086d009dc9cf377
landonbuell <lhb1007@wildcats.unh.edu>
2020-10-06 13:09:22 -0400

    Minor Updates to Classifier
    - Design Matrix still unstable, needs further development

--- SignalClassifier/ClassifierMain/MachineLearningUtilities.py
+++ SignalClassifier/ClassifierMain/MachineLearningUtilities.py
@@ -59,9 +59,7 @@ class DesignMatrix:
     def ShapeBySample (self):
         """ Reshape design matrix by number of samples """
         self.X = np.array(self.X)
-        sampleShape = self.X[0].shape       # shape of 1 sample
-        newShape = [self.n_samples] + [i for i in sampleShape]
-        self.X = self.X.reshape(newShape)
+        self.X.reshape(self.n_samples,-1)
         return self
 
     def SetMatrixData(self,X_new):
@@ -70,7 +68,7 @@ class DesignMatrix:
         return self
 
     def __Get_Y__(self,onehot=True):
-        """ treturn target matrix as One-hot-enc matrix """
+        """ Return target matrix as One-hot-enc matrix """
         if onehot == True:
             self.Y = keras.utils.to_categorical(self.targets,self.n_classes)
         return self.Y
