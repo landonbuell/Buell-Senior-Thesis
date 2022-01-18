b562e269d877924bacd9c4dea4e30907ca37bc9a
LandonBuell <42503615+landonbuell@users.noreply.github.com>
2022-01-17 22:52:06 -0500

    Updates to Preprocessing Tool Project

--- SignalClassifierPrototype/FeatureEngineering/Preprocessing.py
+++ SignalClassifierPrototype/FeatureEngineering/Preprocessing.py
@@ -11,16 +11,76 @@ Date:           January 2022
         #### IMPORTS ####
 
 import numpy as np
+from numpy.lib.function_base import average
 
 import CommonStructures
 
         #### CLASS DEFINTIONS ####
 
+class PreprocessingTool:
+    """ Abstract Parent Class For all Preprocessing Tools """
+
+    def __init__(self,toolName):
+        """ Constructor for PreprocessingTool Abstract Class """
+        self._toolName = toolName
+        self._timesFit = 0
+        self._sampleShape = []
+        
+    def __del__(self):
+        """ Destruction for PreprocessingTool Abstract Class """
+        pass
+
+    # Getters and Setters
+
+    def getToolName(self):
+        """ Return the name of this tool """
+        return self._toolName
+
+    def getTimesFit(self):
+        """ Get the Number of times that the tool has been fit """
+        return self._timesFit
+
+    def getIsFit(self):
+        """ Return T/F if the model has been fit at all """
+        return (self._timesFit > 0)
+
+    def getSampleShape(self):
+        """ Get the Shape of Each Sample """
+        return self._sampleShape
+
+    # Public Interfacce
+
+    def fit(self,designMatrix):
+        """ Fit the Preprocessing Tool w/ a design matrix Object """
+        return self
+
+    def transform(self,designMatrix):
+        """ Transform a design matrix with the fir params """
+        return designMatrix
+
+    def reset(self):
+        """ Reset the state of the tool """
+        self._timesFit = 0
+        self._sampleShape = []
+        return self
+
+    # Magic Methods
+
+    def __repr__(self):
+        """ Return Debugger Representation of Instance """
+        return str(self.__class__) + " @ " + str(hex(id(self)))
+
 class SelectKBest:
     """ Select K Features that score best by metric """
     
-    def __init__(self):
+    def __init__(self,scoringCallback):
         """ Constructor for SelectKBest """
+        super().__init__("SelectKBest")
+        self._scoringCallback = scoringCallback
+
+    def __del__(self):
+        """ Destructor for FeatureScaler Instance """
+        pass
 
 
 class FeatureScaler:
@@ -28,75 +88,74 @@ class FeatureScaler:
 
     def __init__(self):
         """ Constructor for FeatureScaler Instance """
-        self._sampleShape = []
-        self._matrixType = "-1"
-        self._isFitted = False
-        self._means = np.array([])
-        self._varis = np.array([])
+        super().__init__("FeatureScaler")
+        self._historicalWeights
+        self._means = np.array([],dtype=np.float32)
+        self._varis = np.array([],dtype=np.float32)
 
     def __del__(self):
         """ Destructor for FeatureScaler Instance """
-        self._runInfo = None
+        pass
 
     # Getters and Setters
 
-    def getNumFeatures(self):
-        """ Return the Number of Features in the Design Matrix """
-        return self._numFeatures
+    def getAverageOfMeans(self):
+        """ Compute the Average of all batch means """
+        return np.mean(self._means,axis=0,dtype=np.float32)
 
-    def getMeans(self):
-        """ Get the Variance of Each Feature """
-        return self._means
-
-    def getVariances(self):
-        """ Return the Variance of Each Feature """
+    def getAverageOfVariances(self):
+        """ Compute the Average of all batch variances """
+        return np.mean(self._varis,axis=0,dtype=np.float32)
 
     # Public Interface
 
-    def fitRunInfo(self,runInfoInstance,matrixType="A"):
-        """ Fit the Scaler by Using a RunInfo Instance (Larger DataSets) """
-        if (matrixType == "A"):
-            # Type A Matrix
-            self._sampleShape = np.array(runInfoInstance.getShapeSampleA())    
-            self._matrixType = "A"
-        elif( matrixType == "B"):
-            # Type B Matrix
-            self._sampleShape = np.array(runInfoInstance.getShapeSampleB())
-            self._matrixType = "B"
-        else:
-            # Bad Matrix Type
-            raise ValueError("matrixType must be A or B!")
+    def fit(self,designMatrix):
+        """ Fit the Preprocessing Tool w/ a design matrix Object """
+        featureMeans = self.computeBatchMeans(designMatrix)
+        featureVaris = self.computeBatchVaris(designMatrix)
+        self.storeBatchMeans(featureMeans)
+        self.storeBatchVaris(featureVaris)
+        
+        self._timesFit += 1
         return self
 
-    def fitDesignMatrix(self,designMatrix):
-        """ Fit the Scaler by Using a DesignMatrix Instance (Smaller DataSets) """
-        self._sampleShape = designMatrix.getSampleShape()
-        self._means = np.mean(designMatrix.getFeatures(),axis=0,dtype=np.float32)
-        self._varis = np.var(designMatrix.getFeatures(),axis=0,dtype=np.float32)
-        self._isFitted = True
-        return self
-
-    def transform(self,designMatrix,verbose=True):
-        """ Transform Design Matrix According to Scaling Parameters """
-        if (self._isFitted == False):
-            raise RuntimeError("Scaler not yet fit")
-        dataRef = designMatrix.getFeatures()
-        dataRef = (dataRef - self._means) / np.sqrt(self._varis)
-        designMatrix.setFeatures(dataRef)
-
-        # Print to Console?
-        if (verbose == True):
-            print(np.mean(designMatrix.getFeatures(),axis=0,dtype=np.float32))
-            print(np.var(designMatrix.getFeatures(),axis=0,dtype=np.float32))
+    def transform(self,designMatrix):
+        """ Transform a design matrix with the fir params """
+        if (self.getIsFit() == False):
+            # Tool not yet Fit
+            raise RuntimeError(repr(self) + " is not yet fit")
         return designMatrix
 
+    # Private Interface
 
-    def clear(self):
-        """ Reset the State of the Instance """
-        self._sampleShape = []
-        self._matrixType = "-1"
-        self._isFitted = False
-        self._means = np.array([])
-        self._varis = np.array([])
+    def computeBatchMeans(self,X):
+        """ Compute the Average of Each Feature in the given batch """
+        return np.mean(X.getFeatures(),axis=0,dtype=np.float32)
+        
+    def computeBatchVaris(self,X):
+        """ Compute the Variance of Each Feature in the given batch """
+        return np.var(X.getFeatures(),axis=0,dtype=np.float32)
+   
+    def storeBatchMeans(self,x):
+        """ Store the average of each feature in this batch """
+        if (self._means.shape[0] == 0):
+            self._sampleShape = list(x.shape)
+        if (x.shape != self._sampleShape):
+            raise ValueError("Shape Mismatch")
+        newShape = [self._means.shape[0]] + self._sampleShape
+        self._means = np.append(self._means,x)
+        self._means.reshape(newShape)
         return self
+
+    def storeBatchVaris(self,x):
+        """ Store the variances of each feature in this batch """
+        if (self._varis.shape[0] == 0):
+            self._sampleShape = list(x.shape)
+        if (x.shape != self._sampleShape):
+            raise ValueError("Shape Mismatch")
+        newShape = [self._varis.shape[0]] + self._sampleShape
+        self._varis = np.append(self._varis,x)
+        self._varis.reshape(newShape)
+        return self
+
 
